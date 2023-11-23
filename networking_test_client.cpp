cc0c3a4af9686d431547f9303c629f3776de3693
Moritz Magdowski <moritz.magdowski@gmail.com>
2023-11-23 17:52:47 +0100

    sending messages from client to host

--- networking_test_client.cpp
+++ networking_test_client.cpp
@@ -10,15 +10,19 @@ using namespace std;
 
 int main()
 {
-    // Create a socket and connect it to 192.168.1.50 on port 55001
-    sf::TcpSocket socket;
-    socket.connect("192.168.1.50", 55001);
-    // Send a message to the connected host
-    std::string message = "Hi, I am a client";
-    socket.send(message.c_str(), message.size() + 1);
-    // Receive an answer from the server
-    char buffer[1024];
-    std::size_t received = 0;
-    socket.receive(buffer, sizeof(buffer), received);
-    std::cout << "The server said: " << buffer << std::endl;
+    // Create a socket and connect it to 192.168.178.62 on port 55001
+    TcpSocket socket;
+    socket.connect("192.168.178.62", 55001);
+    std::cout << "Connected to host: " << "192.168.178.62" << endl;
+
+    string message = "";
+    while (message != "q")
+    {
+        // input a message
+        cin >> message;
+        // send a message to the connected host
+        socket.send(message.c_str(), message.size() + 1);
+    }
+    // press any key to proceed
+    cin.ignore();
 }
--- networking_test_host.cpp
+++ networking_test_host.cpp
@@ -16,13 +16,21 @@ int main()
     // Wait for a connection
     sf::TcpSocket socket;
     listener.accept(socket);
-    std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
-    // Receive a message from the client
-    char buffer[1024];
-    std::size_t received = 0;
-    socket.receive(buffer, sizeof(buffer), received);
-    std::cout << "The client said: " << buffer << std::endl;
-    // Send an answer
-    std::string message = "Welcome, client";
-    socket.send(message.c_str(), message.size() + 1);
+    std::cout << "New client connected: " << socket.getRemoteAddress() << endl;
+
+    bool proceed;
+    while (proceed)
+    {
+        // receive a message from the client
+        char buffer[1024];
+        size_t received = 0;
+        socket.receive(buffer, sizeof(buffer), received);
+        cout << "The client said: " << buffer << endl;
+        if (buffer == "q")
+        {
+            proceed = false;
+        }
+    }
+    // press any key to proceed
+    cin.ignore();
 }
